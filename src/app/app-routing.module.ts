c9fb682fe9d724439b6db4a457efcf0046583f5b
kr-luis <kluis816@gmail.com>
2024-11-15 15:25:07 -0500

    Añadiendo autenticacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,34 +1,27 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-
+import { canActivate, redirectUnauthorizedTo, redirectLoggedInTo } from '@an
+// Send unauthorized users to login
+const redirectUnauthorizedToLogin = () =>
+ redirectUnauthorizedTo(['/']);
+// Automatically log in users
+const redirectLoggedInToChat = () => redirectLoggedInTo(['/chat']);
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-  },
-  {
-    path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
-  },
-  {
-    path: 'chat',
-    loadChildren: () => import('./pages/chat/chat.module').then( m => m.ChatPageModule)
-  },
+ {
+ path: '',
+ loadChildren: () => import('./pages/login/login.module').then( m => m.Lo
+ ...canActivate(redirectLoggedInToChat),
+ },
+ {
+ path: 'chat',
+ ...canActivate(redirectUnauthorizedToLogin),
+ loadChildren: () => import('./pages/chat/chat.module').then( m => m.Chat
+ }
 ];
-
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
+ imports: [
+ RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+ ],
+ exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,12 @@
 export const environment = {
   production: false,
-  firebaseConfig: {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyCgQQUH9QiuFfwBDl0ZYlnHrINSphf5-kw",
+  authDomain: "app-movil-23967.firebaseapp.com",
+  projectId: "app-movil-23967",
+  storageBucket: "app-movil-23967.firebasestorage.app",
+  messagingSenderId: "1020846522981",
+  appId: "1:1020846522981:web:a1e12552c8c0e234febc8c",
+  measurementId: "G-KZ30H0R267"
   }
  };
  
